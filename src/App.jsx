7d238fe09d33c60730144b598ac82e3d08dc7ab2
ArtVelych <artemvelychko1999@gmail.com>
2025-04-24 22:08:32 +0200

    Lesson 2 Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 // =========================================================================== Task 1 ====================================
@@ -15,6 +15,8 @@ import "./App.css";
 //   );
 // };
 
+// export default App;
+
 // ====================================================================== Task 2: Об'єкт події ====================================
 // function App() {
 //   const handleClick = (evt) => {
@@ -27,7 +29,9 @@ import "./App.css";
 //       <button onClick={(evt) => console.log(evt)}>Second button</button>
 //     </>
 //   );
-// }
+// };
+
+// export default App;
 
 // =========================================================================== Task 3: Reading props ====================================
 // const CustomButton = ({ message, children }) => {
@@ -43,6 +47,8 @@ import "./App.css";
 //   );
 // };
 
+// export default App;
+
 // =========================================================================== Task 4: State ====================================
 
 // const App = () => {
@@ -54,6 +60,8 @@ import "./App.css";
 
 //   return <button onClick={handleClick}>Current: {clicks}</button>;
 // };
+
+// export default App;
 
 // =========================================================================== Task 5: Several States ====================================
 // function App() {
@@ -77,6 +85,8 @@ import "./App.css";
 //   );
 // }
 
+// export default App;
+
 // =========================================================================== Task 6: States are Isolated: Examples ====================================
 // const ClickCounter = () => {
 //   const [clicks, setClicks] = useState(0);
@@ -96,6 +106,8 @@ import "./App.css";
 //     </>
 //   );
 // };
+
+// export default App;
 
 // =========================================================================== Task 7: Synchronised States ====================================
 // ClickCounter отримує функцію onUpdate (ім'я пропа),
@@ -121,37 +133,311 @@ import "./App.css";
 //   );
 // };
 
+// export default App;
+
 // =========================================================================== Task 8: Updating Objects ====================================
+// const App = () => {
+//   const [values, setValues] = useState({
+//     x: 0,
+//     y: 0,
+//   });
+
+//   const updateX = () => {
+//     setValues({
+//       ...values,
+//       x: values.x + 1,
+//     });
+//   };
+
+//   const updateY = () => {
+//     setValues({
+//       ...values,
+//       y: values.y + 1,
+//     });
+//   };
+
+//   return (
+//     <div>
+//       <p>
+//         x: {values.x}, y: {values.y}
+//       </p>
+
+//       <button onClick={updateX}>Update x</button>
+//       <button onClick={updateY}>Update y</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// =========================================================================== Task 9: useEffect ====================================
+// const App = () => {
+//   const [clicks, setClicks] = useState(0);
+
+//   // Оголошуємо ефект
+//   useEffect(() => {
+//     document.title = `You clicked ${clicks} times`;
+//   });
+
+//   return (
+//     <button onClick={() => setClicks(clicks + 1)}>
+//       You clicked {clicks} times
+//     </button>
+//   );
+// };
+
+// export default App;
+
+// =========================================================================== Task 10.1: useEffect with array ====================================
+// const App = () => {
+//   const [clicks, setClicks] = useState(0);
+
+//   useEffect(() => {
+//     console.log("You can see me only once!");
+//   }, []);
+
+//   return (
+//     <button onClick={() => setClicks(clicks + 1)}>
+//       You clicked {clicks} times
+//     </button>
+//   );
+// };
+
+// export default App;
+
+// =========================================================================== Task 10.2: useEffect with array ====================================
+
+// const App = () => {
+//   useEffect(() => {
+//     setInterval(() => {
+//       console.log(`Interval - ${Date.now()}`);
+//     }, 2000);
+//   }, []);
+
+//   return <div>App</div>;
+// };
+
+// export default App;
+
+// =========================================================================== Task 10.3: useEffect ====================================
+// const Modal = () => {
+//   useEffect(() => {
+//     setInterval(() => {
+//       console.log(`Interval - ${Date.now()}`);
+//     }, 2000);
+//   }, []);
+
+//   return <div>Modal</div>;
+// };
+
+// const App = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   return (
+//     <div>
+//       <button onClick={() => setIsOpen(!isOpen)}>
+//         {isOpen ? "Close" : "Open"}
+//       </button>
+//       {isOpen && <Modal />}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// =========================================================================== Task 10.4: clearInterval ====================================
+
+// const Modal = () => {
+//   useEffect(() => {
+//     // Зберігаємо ідентифікатор інтервалу в змінну
+//     const intervalId = setInterval(() => {
+//       console.log(`Interval - ${Date.now()}`);
+//     }, 2000);
+
+//     return () => {
+//       // Очищаємо інтервал за його ідентифікатором
+//       clearInterval(intervalId);
+//     };
+//   }, []);
+
+//   return <div>Modal</div>;
+// };
+
+// const App = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   return (
+//     <div>
+//       <button onClick={() => setIsOpen(!isOpen)}>
+//         {isOpen ? "Close" : "Open"}
+//       </button>
+//       {isOpen && <Modal />}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// =========================================================================== Task 11: useEffect Dependencies ====================================
+
+// const App = () => {
+//   const [clicks, setClicks] = useState(0);
+
+//   // ✅ Залежності вказані правильно
+//   useEffect(() => {
+//     console.log("Clicks updated: ", clicks);
+//   }, [clicks]);
+
+//   return (
+//     <button onClick={() => setClicks(clicks + 1)}>
+//       You clicked {clicks} times
+//     </button>
+//   );
+// };
+
+// export default App;
+
+// =========================================================================== Task 12.1: Several Effects ====================================
+// const App = () => {
+//   const [clicks, setClicks] = useState(0);
+
+//   useEffect(() => {
+//     console.log("You can see me only once!");
+//   }, []);
+
+//   useEffect(() => {
+//     console.log("Clicks updated: ", clicks);
+//   }, [clicks]);
+
+//   useEffect(() => {
+//     document.title = `You clicked ${clicks} times`;
+//   });
+
+//   return (
+//     <button onClick={() => setClicks(clicks + 1)}>
+//       You clicked {clicks} times
+//     </button>
+//   );
+// };
+
+// export default App;
+
+// =========================================================================== Task 12.1: Several Effects ====================================
+// const App = () => {
+//   const [first, setFirst] = useState(0);
+//   const [second, setSecond] = useState(0);
+
+//   useEffect(() => {
+//     console.log("First updated: ", first);
+//   }, [first]);
+
+//   useEffect(() => {
+//     console.log("Second updated: ", second);
+//   }, [second]);
+
+//   useEffect(() => {
+//     console.log("First or second updated: ", first + second);
+//   }, [first, second]);
+
+//   return (
+//     <>
+//       <button onClick={() => setFirst(first + 1)}>First: {first}</button>
+//       <button onClick={() => setSecond(second + 1)}>Second: {second}</button>
+//     </>
+//   );
+// };
+
+// export default App;
+
+// =========================================================================== Task 13.1: Reset ====================================
+// const App = () => {
+//   const [clicks, setClicks] = useState(0);
+
+//   return (
+//     <div>
+//       <button onClick={() => setClicks(clicks + 1)}>
+//         You clicked {clicks} times
+//       </button>
+//       <button onClick={() => setClicks(0)}>Reset</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// =========================================================================== Task 13.2: Reset ====================================
+// const App = () => {
+//   const [clicks, setClicks] = useState(0);
+
+//   useEffect(() => {
+//     console.log(`Clicks changed - ${clicks}`);
+//   }, [clicks]);
+
+//   return (
+//     <div>
+//       <button onClick={() => setClicks(clicks + 1)}>
+//         You clicked {clicks} times
+//       </button>
+//       <button onClick={() => setClicks(0)}>Reset</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// =========================================================================== Task 14.1: LocalStorage ====================================
+// const App = () => {
+//   const [clicks, setClicks] = useState(0);
+
+//   useEffect(() => {
+//     window.localStorage.setItem("saved-clicks", clicks);
+//   }, [clicks]);
+
+//   return (
+//     <div>
+//       <button onClick={() => setClicks(clicks + 1)}>
+//         You clicked {clicks} times
+//       </button>
+//       <button onClick={() => setClicks(0)}>Reset</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// !!! If working with an object or array, use: window.localStorage.setItem("key", JSON.stringify({}));
+
+// =========================================================================== Task 14.2: LocalStorage ====================================
 const App = () => {
-  const [values, setValues] = useState({
-    x: 0,
-    y: 0,
+  const [clicks, setClicks] = useState(() => {
+    const savedClicks = window.localStorage.getItem("saved-clicks");
+    if (savedClicks !== null) {
+      return Number(savedClicks);
+    }
+    return 0;
   });
 
-  const updateX = () => {
-    setValues({
-      ...values,
-      x: values.x + 1,
-    });
-  };
-
-  const updateY = () => {
-    setValues({
-      ...values,
-      y: values.y + 1,
-    });
-  };
+  useEffect(() => {
+    window.localStorage.setItem("saved-clicks", clicks);
+  }, [clicks]);
 
   return (
     <div>
-      <p>
-        x: {values.x}, y: {values.y}
-      </p>
-
-      <button onClick={updateX}>Update x</button>
-      <button onClick={updateY}>Update y</button>
+      <button onClick={() => setClicks(clicks + 1)}>
+        You clicked {clicks} times
+      </button>
+      <button onClick={() => setClicks(0)}>Reset</button>
     </div>
   );
 };
 
 export default App;
+
+// If working with an object or array, use:
+// const [obj, setObj] = useState(() => {
+// const savedObject = window.localStorage.getItem("key");
+// if (savedObject !== null) {
+// return JSON.parse(savedObject);
+// }
+// return {};
+// });
